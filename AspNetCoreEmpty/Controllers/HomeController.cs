4aa60e85420fb8c9be294444a538654f663df88c
GÖZ'SS <GZ'SS@DESKTOP-NKTDPLS>
2024-09-05 18:18:18 +0300

    Add project files.

---
+++ AspNetCoreEmpty/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        // bu metot Index view'nin dönmesini sağlar.
+        // IActionResult  = viewlerin tipi IAction olduğu için bu method çağrılır. Controller'ın içindeki metotlara action denir.
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCoreEmpty/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreEmpty.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="Kullanıcı Adı Gereklidir'") ]
+        [StringLength(150)]
+        public string UserName { get; set; }
+
+
+    }
+}
